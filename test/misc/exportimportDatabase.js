076f9edbbf3294af99b527f3a798385d04d41fdf
Anand Pahuja <anand_pahuja@yahoo.com>
2017-10-30 12:12:35 +0530

    Import Export Database

--- test/misc/exportimportDatabase.js
+++ test/misc/exportimportDatabase.js
@@ -62,7 +62,7 @@ describe("Export & Import Database", function () {
     });
 
     it("Export Database and Import Database", function (done) {
-        this.timeout(50000);
+        this.timeout(100000);
         var default_count = 0;
         var table_count = 0;
         var exportUrl = CB.apiUrl + "/backup/" + CB.appId + "/exportdb";
